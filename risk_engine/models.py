ce0241682bedaa19a043d31e3d7d3505316bd1f8
DThomas-Statistics <DafyddThomas.maths@outlook.com>
2026-07-26 09:54:24 +0100

    Fix power parameter retrieval in GARCH model
    Updated GARCH model to use 'power' attribute from volatility.

--- risk_engine/models.py
+++ risk_engine/models.py
@@ -130,7 +130,8 @@ def findGARCH(percent_vec):
 def compute_garch_risk(o_model, alpha,window):
   vol_type = o_model.model.volatility
   dist_type = o_model.model.distribution
-  Model = arch_model(window,mean = 'constant', lags=0, vol = vol_type, p=o_model.model.volatility.p, q=o_model.model.volatility.q, o=o_model.model.volatility.o, power=o_model.model.volatility.power, dist = dist_type, rescale = True)
+  power_val = getattr(o_model.model.volatility, 'power', 2.0)
+  Model = arch_model(window,mean = 'constant', lags=0, vol = vol_type, p=o_model.model.volatility.p, q=o_model.model.volatility.q, o=o_model.model.volatility.o, power=power_val, dist = dist_type, rescale = True)
   res=Model.fit()
   vol_type = res.model.volatility
   #DF = res.params['nu']
