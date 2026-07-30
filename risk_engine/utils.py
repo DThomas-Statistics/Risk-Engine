532cfac8ec7730b183fb64f4d0fe7e7a5dfb15a3
DThomas-Statistics <DafyddThomas.maths@outlook.com>
2026-07-30 16:22:32 +0100

    Implement portfolio_vec function in utils.py
    Add portfolio_vec function to calculate portfolio metrics.

--- risk_engine/utils.py
+++ risk_engine/utils.py
@@ -1,1 +1,9 @@
+import numpy as np
+
+def portfolio_vec(weight, price_matrix):
+  percent=(price_matrix[:, 1:] - price_matrix[:, :-1]) / price_matrix[:, :-1]
+  percent[percent == 0.0] = 1e-8
+  port_vec = np.dot(weight,price_matrix)
+  percent_vec = np.dot(weight,percent)
+  return percent,port_vec, percent_vec
 
